8abd2c88ef5edb5e04383e2c74d657805535e0db
subanesh-swe <subaneshtechie@gmail.com>
2024-04-02 17:50:41 +0530

    added features

---
+++ EchoAll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp.Server;
+using WebSocketSharp;
+
+namespace C_Sharp_WebSocket_Server
+{
+    public class EchoAll : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Console.WriteLine("Received message from EchoAll client: " + e.Data);
+            Sessions.Broadcast(e.Data);
+        }
+    }
+}
