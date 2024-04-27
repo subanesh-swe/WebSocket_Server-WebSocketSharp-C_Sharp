678848cae9e51536fe17c3222ce4a6bd3550120c
subanesh-swe <subaneshtechie@gmail.com>
2024-04-27 19:33:54 +0530

    added features

---
+++ Database.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_WebSocket_Server
+{
+    public class Database
+    {
+        public static Dictionary<string, string> clientDict = new Dictionary<string, string>();
+        // key: name of client, 
+        // value: session id of client
+    }
+}
