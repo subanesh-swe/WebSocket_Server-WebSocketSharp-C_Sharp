e4a4193f3cdeb601a6a5852db0e89287e3fcb84f
subanesh-swe <subaneshtechie@gmail.com>
2024-04-29 15:34:58 +0530

    added features

--- Database.cs
+++ Database.cs
@@ -11,5 +11,16 @@ namespace C_Sharp_WebSocket_Server
         public static Dictionary<string, string> clientDict = new Dictionary<string, string>();
         // key: name of client, 
         // value: session id of client
+
+        public static void Add(string key, string value)
+        {
+            clientDict.Add(key, value);
+        }
+
+        public static void Remove(string key, string value)
+        {
+            clientDict.Remove(key);
+        }
+
     }
 }
